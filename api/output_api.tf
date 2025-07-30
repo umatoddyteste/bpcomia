5d65a3520c13029c5496b32bb0452eb17d9c1bd1
umatoddyteste <contatwitterlj@gmail.com>
2025-07-30 15:53:12 -0300

    Create output_api.tf

---
+++ api/output_api.tf
@@ -0,0 +1,11 @@
+output "id" {
+  value = aws_api_gateway_rest_api.this.id
+}
+
+output "rest_api_id" {
+  value = aws_api_gateway_rest_api.this.id
+}
+
+output "root_resource_id" {
+  value = aws_api_gateway_rest_api.this.root_resource_id
+}
